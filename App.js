16082e2614aff49f9c0e45a9d0e03503773692ce
DangFPT <dangnvpd09149@fpt.edu.vn>
2024-02-23 15:22:47 +0700

    commit

--- App.js
+++ App.js
@@ -3,9 +3,11 @@ import {Text} from 'react-native';
 import Lab3 from './src/screen/lab3';
 import Lab4 from './src/screen/lab4';
 import Lab5 from './src/screen/lab5';
+import Lab6 from './src/screen/lab6';
+import Lab7 from './src/screen/lab7';
 
 const App = () => {
-  return <Lab5/>;
+  return <Lab7/>;
 };
 
 export default App;
